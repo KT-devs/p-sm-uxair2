a5fd899b1938ec5920155b508090ba86ee3820ef
Sergey Melnikov <pdasilem@gmail.com>
2023-03-06 22:52:47 +0400

    Initial commit

---
+++ flight-service/src/main/java/ru/uxair1/flight-service/util/exceptions/AliasConstants.java
@@ -0,0 +1,13 @@
+package ru.uxair1.apigateway.util.exceptions;
+
+public final class AliasConstants {
+
+    public static final String USER = "User";
+    public static final String GUEST = "Guest";
+    public static final String ROLE = "Role";
+    public static final String PERMISSION = "Permission";
+
+    private AliasConstants() {
+        throw new AssertionError("Suppress default constructor for non instantiability");
+    }
+}
---
+++ notification-service/src/main/java/ru/uxair1/notification-service/util/exceptions/Base64DecodeException.java
@@ -0,0 +1,19 @@
+package ru.uxair1.flight;
+
+public class Base64DecodeException  extends RuntimeException{
+
+    private final String message;
+
+    public Base64DecodeException(final String argument) {
+        this.message = buildMessage(argument);
+    }
+
+    @Override
+    public String getMessage() {
+        return message;
+    }
+
+    private String buildMessage(final String argument) {
+        return "Param was not URL-safe encoded: " + argument;
+    }
+}
