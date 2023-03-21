4aafe11d89227873cc771e0ed7dd4de984ed8dc1
GK2128 <puyol2128@mail.ru>
2023-03-21 18:11:52 +0300

    v1.1

--- flight-service/src/main/java/ru/uxair/flight/entity/BookingStatus.java
+++ flight-service/src/main/java/ru/uxair/flight/entity/BookingStatus.java
@@ -1,7 +1,15 @@
 package ru.uxair.flight.entity;
 
 public enum BookingStatus {
-    RESERVED,
-    PAID,
-    CANCELED
+    RESERVED("Зарезервировано"), PAID("Оплачено"), CANCELED("Отменено");
+
+    private String value;
+
+    private BookingStatus(String value) {
+        this.value = value;
+    }
+
+    public String getValue() {
+        return value;
+    }
 }
