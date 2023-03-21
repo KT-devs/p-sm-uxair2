a537c6fc3a97f24a0f0ad45b0523cf82f0d52be6
GK2128 <puyol2128@mail.ru>
2023-03-21 18:03:30 +0300

    v1.1

--- user-management/src/main/java/ru/uxair/user/entity/DocumentType.java
+++ user-management/src/main/java/ru/uxair/user/entity/DocumentType.java
@@ -1,6 +1,13 @@
 package ru.uxair.user.entity;
 
 public enum DocumentType {
-    NATIONAL_PASSPORT,
-    INTERNATIONAL_PASSPORT
+    NATIONAL_PASSPORT("Пасспорт"), INTERNATIONAL_PASSPORT("Международный пасспорт");
+    private String value;
+    private DocumentType(String value) {
+        this.value = value;
+    }
+
+    public String getValue() {
+        return value;
+    }
 }
