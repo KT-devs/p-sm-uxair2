934059033c7576c78266a6ece79aac931a74af27
Vadim <vsm_mil@inbox.ru>
2023-03-21 18:56:07 +0300

    UXAR2-9 update mapper

--- flight-service/src/main/java/ru/uxair/flight/util/mapper/DestinationMappingImp.java
+++ flight-service/src/main/java/ru/uxair/flight/util/mapper/DestinationMappingImp.java
@@ -1,5 +1,6 @@
 package ru.uxair.flight.util.mapper;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
 import ru.uxair.flight.entity.Destination;
 import ru.uxair.flight.entity.Dto.DestinationDto;
@@ -17,15 +18,8 @@ public class DestinationMappingImp implements DestinationMapping {
      */
     @Override
     public DestinationDto mapToDestinationDto(Destination destinationEntity) {
-        DestinationDto destinationDto = new DestinationDto();
-        destinationDto.setId(destinationEntity.getId());
-        destinationDto.setCity(destinationEntity.getCity());
-        destinationDto.setCountryName(destinationEntity.getCountryName());
-        destinationDto.setCountryCode(destinationEntity.getCountryCode());
-        destinationDto.setAirportName(destinationEntity.getAirportName());
-        destinationDto.setAirportCode(destinationEntity.getAirportCode());
-        destinationDto.setTimezone(destinationEntity.getTimezone());
-        return destinationDto;
+        ModelMapper modelMapper = new ModelMapper();
+        return modelMapper.map(destinationEntity, DestinationDto.class);
     }
 
     /**
@@ -35,14 +29,7 @@ public class DestinationMappingImp implements DestinationMapping {
      */
     @Override
     public Destination mapToDestinationEntity(DestinationDto destinationDto) {
-        Destination destination = new Destination();
-        destination.setId(destinationDto.getId());
-        destination.setCity(destinationDto.getCity());
-        destination.setCountryName(destinationDto.getCountryName());
-        destination.setCountryCode(destinationDto.getCountryCode());
-        destination.setAirportName(destinationDto.getAirportName());
-        destination.setAirportCode(destinationDto.getAirportCode());
-        destination.setTimezone(destinationDto.getTimezone());
-        return destination;
+        ModelMapper modelMapper = new ModelMapper();
+        return modelMapper.map(destinationDto, Destination.class);
     }
 }
