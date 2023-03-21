a4d465c704a0d64fbdce8f9398d4f6fe22f5fbc6
GK2128 <puyol2128@mail.ru>
2023-03-21 18:10:15 +0300

    v1.1

--- flight-service/src/main/java/ru/uxair/flight/entity/WeekDay.java
+++ flight-service/src/main/java/ru/uxair/flight/entity/WeekDay.java
@@ -1,11 +1,16 @@
 package ru.uxair.flight.entity;
 
 public enum WeekDay {
-    MONDAY,
-    TUESDAY,
-    WEDNESDAY,
-    THURSDAY,
-    FRIDAY,
-    SATURDAY,
-    SUNDAY
+    MONDAY("Понедельник"), TUESDAY("Вторник"), WEDNESDAY("Среда"), THURSDAY("Четверг"), FRIDAY("Пятница"),
+    SATURDAY("Суббота"), SUNDAY("Воскресенье");
+
+    private String value;
+
+    private WeekDay(String value) {
+        this.value = value;
+    }
+
+    public String getValue() {
+        return value;
+    }
 }
