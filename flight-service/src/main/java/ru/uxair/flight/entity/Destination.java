0f4cb4bd342b1540c803e64e0c536dac2aacc0ba
Vadim <vsm_mil@inbox.ru>
2023-03-23 18:07:37 +0300

    UXAR2-9 added validation

--- flight-service/src/main/java/ru/uxair/flight/entity/Destination.java
+++ flight-service/src/main/java/ru/uxair/flight/entity/Destination.java
@@ -3,12 +3,8 @@ package ru.uxair.flight.entity;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import org.hibernate.validator.constraints.Length;
-
 import javax.persistence.*;
-import javax.validation.constraints.Max;
-import javax.validation.constraints.Min;
-import java.time.LocalDateTime;
+import javax.validation.constraints.*;
 
 /**
  * Сущность Направление
@@ -23,30 +19,34 @@ public class Destination {
     @Column(name = "id")
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
+    @NotEmpty(message = "City should not be empty")
+    @Size(min = 1, max = 255, message = "City should be between 1 and 255 characters")
     @Column(name = "city", nullable = false)
-    @Length(min = 1, max = 255)
     private String city;
+    @NotEmpty(message = "Country name should not be empty")
+    @Size(min = 1, max = 255, message = "countryName should be between 1 and 255 characters")
     @Column(name = "country_name", nullable = false)
-    @Length(min = 1, max = 255)
     private String countryName;
+    @Min(value = 1, message = "Country code cannot be less than 1")
+    @Max(value = 999, message = "Country code cannot be more than 999")
     @Column(name = "country_code", nullable = false)
-    @Min(1)
-    @Max(999)
     private int countryCode;
+    @NotEmpty(message = "Airport name should not be empty")
+    @Size(min = 1, max = 255, message = "Airport name should be between 1 and 255 characters")
     @Column(name = "airport_name", unique = true, nullable = false)
-    @Length(min = 1, max = 255)
     private String airportName;
+    @Min(value = 1, message = "Airport code cannot be less than 1")
+    @Max(value = 999, message = "Airport code cannot be more than 999")
     @Column(name = "airport_code", unique = true, nullable = false)
-    @Min(1)
-    @Max(999)
     private int airportCode;
     /**
      * UTC - Всемирное координированное время
      * пример в БД UTC+3 - это Московское
      *             UTC+9:30 - Южная Австралия, Северная территория
      */
+    @NotEmpty(message = "Timezone should not be empty")
+    @Size(min = 5, max = 9, message = "Timezone should be between 5 and 9 characters")
     @Column(name = "timezone", nullable = false)
-    @Length(min = 5, max = 9)
     private String timezone;
 
 }
