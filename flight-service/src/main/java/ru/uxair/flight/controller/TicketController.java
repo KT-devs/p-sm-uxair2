a4679a6d83e86aac14c8b28be48b6aedbf713f96
WDissney <denrom1990@yandex.ru>
2023-03-23 19:24:40 +0300

    change 1.1

--- flight-service/src/main/java/ru/uxair/flight/controller/TicketController.java
+++ flight-service/src/main/java/ru/uxair/flight/controller/TicketController.java
@@ -1,28 +1,46 @@
 package ru.uxair.flight.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import ru.uxair.flight.entity.Ticket;
 
 import java.util.List;
+@Tag(name = "Билет", description = "API управления билетами")
 @RequestMapping("/api/flight-service/ticket")
 public interface TicketController {
+    @Operation(summary = "Список ввсех билетов",
+            description = "Получить список всех билетов из БД")
     @GetMapping
     List<Ticket> getAllTickets();
+    @Operation(summary = "Список билетов по категории",
+            description = "Список билетов по категории (бизнес, эконом и т.д...)")
     @GetMapping("/category/{category}")
     List<Ticket> getTicketsBySeatCategory(@PathVariable String category);
+    @Operation(summary = "Получить билеты по пассажиру",
+            description = "Получить все билеты определенного пассажира")
     @GetMapping("/passenger/{passenger}")
     List<Ticket> getTicketByPassenger(@PathVariable String passenger);
+    @Operation(summary = "Билеты по рейсу",
+            description = "Получить все билеты определенного рейса")
     @GetMapping("/flight/{flight}")
     List<Ticket> getTicketByFlight(@PathVariable String flight);
+    @Operation(summary = "Список билетов сортированный МИН-МАКС стоимость",
+            description = "Получить список билетов отсортированный от минимальной к максимальной цене")
     @GetMapping("/sortMinToMax")
     List<Ticket> getTicketsFareMinToMax();
+    @Operation(summary = "Список билетов сортированный МАКС-МИН стоимость",
+            description = "Получить список билетов отсортированный от максимальной к мнимальной цене")
     @GetMapping("/sortMaxToMin")
     List<Ticket> getTicketsFareMaxToMin();
+    @Operation(summary = "Поиск билета по ID")
     @GetMapping("/{id}")
     Ticket findTicketById(@PathVariable Long id);
+    @Operation(summary = "Сохранить билет в БД")
     @PostMapping("/save")
     ResponseEntity<String> saveTicket(@RequestBody Ticket ticket);
+    @Operation(summary = "Удалить билет из БД")
     @DeleteMapping("/{id}")
     void deleteTicket(@PathVariable Long id);
 
--- flight-service/src/main/java/ru/uxair/flight/entity/Ticket.java
+++ flight-service/src/main/java/ru/uxair/flight/entity/Ticket.java
@@ -1,34 +1,41 @@
 package ru.uxair.flight.entity;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
-
+@Schema(description = "Сущность билета")
 @Entity
 @NoArgsConstructor
 @AllArgsConstructor
 @Data
 public class Ticket {
+    @Schema(description = "Идентификатор")
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id")
     private Long id;
-
-//    @ManyToOne
+    @Schema(description = "Пассажир")
     @Column(name = "passenger")
     private String passenger;
+    @Schema(description = "Бронирование")
     @Column(name = "booking")
     private String booking;
+    @Schema(description = "Категория места в самолете (бизнес, эконом и т.д...)")
     @Column(name = "seat_category")
     private String seatCategory;
+    @Schema(description = "Дополнительные сервисы на рейсе")
     @Column(name = "in_flight_service")
     private String inflightService;
+    @Schema(description = "Стоимость билета")
     @Column(name = "fare")
     private float fare;
+    @Schema(description = "Валюта")
     @Column(name = "currency")
     private String currency;
+    @Schema(description = "Рейс")
     @Column(name = "flight")
     private String flight;
 }
