f461b0aec52a0edad8e2a97943c2e2a6d1481054
WDissney <denrom1990@yandex.ru>
2023-03-25 11:49:20 +0300

    some

--- flight-service/src/main/java/ru/uxair/flight/util/mapper/TicketMapper.java
+++ flight-service/src/main/java/ru/uxair/flight/util/mapper/TicketMapper.java
@@ -5,6 +5,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import ru.uxair.flight.entity.Dto.TicketDto;
 import ru.uxair.flight.entity.Ticket;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
 @Component
 public class TicketMapper {
 
@@ -19,5 +23,11 @@ public class TicketMapper {
     }
     public Ticket convertToTicketEntity(TicketDto ticketDto){
         return modelMapper.map(ticketDto, Ticket.class);
+    }
+
+    public List<TicketDto> convertToListDto(List<Ticket> tickets){
+        return tickets.stream()
+                .map(this::convertToTicketDto)
+                .collect(Collectors.toList());
     }
 }
