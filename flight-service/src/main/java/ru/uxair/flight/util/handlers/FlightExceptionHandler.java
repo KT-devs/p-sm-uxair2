cc59430a5472cd1b4ba1e10849b90f4e0bfe5e5f
Vadim <vsm_mil@inbox.ru>
2023-03-29 16:49:54 +0300

    UXAR2-26 added DestinationExceptionHandler class

--- flight-service/src/main/java/ru/uxair/flight/util/handlers/FlightExceptionHandler.java
+++ flight-service/src/main/java/ru/uxair/flight/util/handlers/FlightExceptionHandler.java
@@ -6,7 +6,6 @@ import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import ru.uxair.flight.util.exceptions.DestinationNotFoundException;
 import ru.uxair.flight.entity.Dto.ErrorResponseDto;
 import javax.validation.ConstraintViolation;
 import javax.validation.ConstraintViolationException;
@@ -15,11 +14,6 @@ import java.util.stream.Collectors;
 
 @ControllerAdvice
 public class FlightExceptionHandler extends AbstractExceptionHandler{
-
-    @ExceptionHandler(DestinationNotFoundException.class)
-    public ResponseEntity<ErrorResponseDto> destinationNotFoundExceptionHandler(DestinationNotFoundException e) {
-        return buildErrorResponse(e, HttpStatus.NOT_FOUND);
-    }
 
     @ExceptionHandler(ConstraintViolationException.class)
     public ResponseEntity<ErrorResponseDto> onConstraintValidationException(ConstraintViolationException e) {
