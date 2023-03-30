2f88a7fa3130d1dc3fd14283afee6f3c62c30b18
WDissney <denrom1990@yandex.ru>
2023-03-30 12:05:24 +0300

    add handler

--- flight-service/src/main/java/ru/uxair/flight/util/handlers/TicketExceptionHandler.java
+++ flight-service/src/main/java/ru/uxair/flight/util/handlers/TicketExceptionHandler.java
@@ -1,5 +1,6 @@
 package ru.uxair.flight.util.handlers;
 
+import org.hibernate.validator.internal.engine.path.PathImpl;
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -8,22 +9,31 @@ import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.webjars.NotFoundException;
 import ru.uxair.flight.entity.Dto.ErrorResponseDto;
+import ru.uxair.flight.util.exceptions.TicketNotFoundException;
+
+import javax.validation.ConstraintViolation;
+import javax.validation.ConstraintViolationException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
 
 @ControllerAdvice
 public class TicketExceptionHandler extends AbstractExceptionHandler {
     @ExceptionHandler
-    private ResponseEntity<ErrorResponseDto> handlerNotFoundTicket(NotFoundException e){
-        ErrorResponseDto errorResponseDto = new ErrorResponseDto(e.getMessage());
-        return new ResponseEntity<>(errorResponseDto, HttpStatus.NOT_FOUND);
+    private ResponseEntity<ErrorResponseDto> handlerNotFoundTicket(TicketNotFoundException e){
+        return buildErrorResponse(e, HttpStatus.NOT_FOUND);
     }
     @ExceptionHandler
     private ResponseEntity<ErrorResponseDto> handlerEmptyResultDataAccess(EmptyResultDataAccessException e){
-        ErrorResponseDto errorResponseDto = new ErrorResponseDto(e.getMessage());
-        return new ResponseEntity<>(errorResponseDto, HttpStatus.NOT_FOUND);
+        return buildErrorResponse(e, HttpStatus.NOT_FOUND);
     }
     @ExceptionHandler
     private ResponseEntity<ErrorResponseDto> httpMessageNotReadableException(HttpMessageNotReadableException e){
-        ErrorResponseDto errorResponseDto = new ErrorResponseDto(e.getMessage());
-        return new ResponseEntity<>(errorResponseDto, HttpStatus.NOT_FOUND);
+        return buildErrorResponse(e, HttpStatus.NOT_FOUND);
+    }
+    @ExceptionHandler
+    private ResponseEntity<ErrorResponseDto> constraintViolationException (ConstraintViolationException e){
+        return buildErrorResponse(e, HttpStatus.BAD_REQUEST);
     }
 }
+
