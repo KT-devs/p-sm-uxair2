f07dd0a81fab7584a497e4ee27bdf5fb1ef48684
reduss21 <kartosferon@gmail.com>
2023-03-30 18:07:30 +0300

    flightSeatRebase

--- flight-service/src/main/java/ru/uxair/flight/controller/SeatControllerInterface.java
+++ flight-service/src/main/java/ru/uxair/flight/controller/SeatControllerInterface.java
@@ -1,4 +1,4 @@
-package ru.uxair1.flight-service.controller;
+package ru.uxair.flight.controller;
 
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.parameters.RequestBody;
